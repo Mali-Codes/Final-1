b550d67801fd281d84c452a3012937749304b142
Malakai <poncemalakai@gmail.com>
2025-12-08 15:53:44 -0800

    building

--- Final_1.cpp
+++ Final_1.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 using namespace std;
 
 int main() {
+
+    ifstream inFile("Flights.txt");
+
+    if (!inFile) {
+        cerr << "not able to open file" << endl;
+        return 1;
+    }
+
+        // Milestone 1: Build the map
+
+    map<string, int> airportTraffic;
+     
+    string origin, destination;
+
+    while (inFile >> origin >> destination) {
+        airportTraffic[origin]++;
+        airportTraffic[destination]++;
+    }
+
+
     
+    
+
 }
